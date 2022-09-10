836d4f9ebd283c8eaafda36837a139bcf990b392
p9 <ke@port9.org>
2022-09-10 11:40:26 +0200

    Update sliverc2-bootstrap.sh

--- sliver-terraform-II-post/sliverc2-bootstrap.sh
+++ sliver-terraform-II-post/sliverc2-bootstrap.sh
@@ -10,12 +10,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -yq install mingw-w64
 
 sleep 3
- MSF nightly framework installer
+#MSF nightly framework installer
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod +x msfinstall
 ./msfinstall
 
- sliver install:
+#sliver install:
 curl https://sliver.sh/install -o sliverc2.sh
 chmod +x sliverc2.sh
 ./sliverc2.sh
@@ -24,4 +24,8 @@ rm /tmp/sliverc2-bootstrap.sh
 rm /tmp/sliverc2.sh
 rm /tmp/terraf*.sh
 systemctl status sliver --no-pager
+# cloudflared
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+sudo dpkg -i cloudflared.deb &&  sudo cloudflared service install eyjbw[INSERT tunnel key here]
+systemctl status cloudflared --no-pager
 exit
